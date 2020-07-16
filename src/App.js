8b0b62c8f6a867c673cf65f0467c9743a25f2c66
codeswithankit <ank8804465648@gmail.com>
2020-07-16 09:55:00 +0530

    pages setup and react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// Pages Import
+import About from './pages/About'
+import Error from './pages/Error'
+import Home from './pages/Home'
+import SingleCocktail from './pages/SingleCocktail'
+// Components
+import NavBar from './components/Navbar'
 
 export default function App() {
-  return <h1>app component</h1>;
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        {/* Error Page */}
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react'
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+        velit labore, dicta ullam repellat ab, quasi eveniet autem vitae maxime
+        alias nostrum quaerat asperiores quae, quis recusandae voluptas
+        consequatur molestiae. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Fugiat illum alias laboriosam repellat, natus
+        similique obcaecati ipsa ducimus numquam vitae perspiciatis ea
+        architecto non atque veritatis accusantium porro dolorem doloremque!
+      </p>
+    </section>
+  )
 }
